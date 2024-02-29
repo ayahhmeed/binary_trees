0257e39c0eb4d661b08b93cf30c92ed54d003c27
ayahhmeed <ayahhmeed@gmail.com>
2024-02-29 10:09:23 -0800

    height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,16 +9,16 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t right_height, left_height;
+	size_t left_height, right_height;
 
 	if (tree == NULL)
 		return (0);
 
-	right_height = binary_tree_height(tree->right);
 	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
-	if (right_height > left_height)
-		return (right_height + 1);
-	else
+	if (left_height > right_height)
 		return (left_height + 1);
+	else
+		return (right_height + 1);
 }

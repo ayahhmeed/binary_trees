ded1e23d56e497bd5fcab3f9aadff82973186e0a
Waad Alshiekh <waad9726@gmail.com>
2024-02-29 18:40:45 -0800

    r

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -8,13 +8,13 @@
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	size_t si = 0;
+	size_t siz = 0;
 
 	if (tree)
 	{
-		si += 1;
-		si += binary_tree_size(tree->left);
-		si += binary_tree_size(tree->right);
+		siz += 1;
+		siz += binary_tree_size(tree->left);
+		siz += binary_tree_size(tree->right);
 	}
-	return (si);
+	return (siz);
 }
